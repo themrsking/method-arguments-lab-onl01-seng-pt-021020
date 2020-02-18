13ab0684de5f1d228b092081f177a3fbb3534c7e
Stefani King <themrsking@gmail.com>
2020-02-18 17:12:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
-def introduction#(name)
-puts "(Hi, my name is #{name}."
+def introduction(name)
+  puts "(Hi, my name is #{name}."
 end
 
-
-
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning program in #{language}."
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning program in #{language}."
   end
